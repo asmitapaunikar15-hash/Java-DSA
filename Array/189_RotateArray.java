9e9dfa980fed5b565f42eaa70c34d29be2227a9f
asmitapaunikar15-hash <asmitapaunikar15@gmail.com>
2026-07-28 23:11:49 +0530

    Implement array rotation in Solution class
    Solved LeetCode 189 : Array rotation

--- Array/189_RotateArray.java
+++ Array/189_RotateArray.java
@@ -1,1 +1,16 @@
-
+class Solution {
+    public void rotate(int[] nums, int k) {
+        int index=0;
+        int n=nums.length;
+        int temp[]=new int[n];
+        k=k%n;
+        for(int i=n-k;i<n;i++){
+            temp[index++]=nums[i];
+        }
+        for(int i=0;i<n-k;i++){
+                temp[index++]=nums[i];
+        }
+        for(int i=0;i<n;i++){
+            nums[i]=temp[i];
+        }
+}}
